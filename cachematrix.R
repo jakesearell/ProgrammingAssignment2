0dddd28a9a54df17efa5c319cc0b02bdb52341de
Jake <110218364+jakesearell@users.noreply.github.com>
2022-08-28 16:31:43 +1200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,17 @@
 
 
 ## This function creates a special "matrix" object that can cache its inverse. 
-
+## This assignment operator "<<-" ensures values persist after the function call
+## and are available later via the parent environment.
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
                 x <<- y
-                inv <<- NULL
+                inv <<- NULL            # set values
         }
-        get <- function() x
-        setinv <- function(inverse) inv <<- inverse
-        getinv <- function() inv 
+        get <- function() x             # get value
+        setinv <- function(inverse) inv <<- inverse     # set value of inverse matrix
+        getinv <- function() inv                        # get value of inverse matrix
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
@@ -21,12 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
                 inv <- x$getinv()
-        if (!is.null(inv)) {
-                message("getting cached data")
+        if (!is.null(inv)) {                            # check if inverse matrix is NULL
+                message("getting cached data")          
                 return(inv)
         }
         data <- x$get()
-        inv <- solve(data, ...)
+        inv <- solve(data, ...)                         # solves for inverse
         x$setinv(inv)
         inv
 }
